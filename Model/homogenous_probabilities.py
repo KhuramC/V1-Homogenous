7a325d56c9e889168ec881829fdeade9e7837a6b
KhuramC <khuramchoudhry123@gmail.com>
2024-03-06 10:02:50 -0600

    added extra clarification in documentation

--- Model/homogenous_probabilities.py
+++ Model/homogenous_probabilities.py
@@ -8,7 +8,7 @@ def find_convergence(uni_connections, dist_num_cell_B, rec_connections=0):
     total connections by the total amount of Cell Type B (both gotten from a distant dependent model). 
     uni_connections: number of unidirectional connections from Cell Type A -> Cell Type B
     dist_num_cell_B: number of Cell Type B in the distant dependent model
-    rec_connections: number of reciprocal connections from the two cells(only applies if cells are of same type)
+    rec_connections: number of reciprocal connections from the two cells(only applies if cells are of same type) 
     """
     total_connections = uni_connections + rec_connections
     convergence = total_connections / dist_num_cell_B
@@ -19,13 +19,13 @@ def homo_edge_probability(convergence, homo_num_cell_A, connect_to_same_type=Fal
     """
     Calculates the probability of a given edge being created in a homogenous context from Cell Type A -> Cell Type B. The convergence is divided by the total number 
     of Cell Type A in the homogenous model to get the probability. If the connection is to a cell of the same type, then it accounts for not wanting a synapse 
-    to itself.
+    to itself. If they are not in the same network (i.e. a virtual Cell Type A connecting to a biophysical Cell Type A), then this exception does not apply.
     convergence: convergence of connections from Cell Type A -> Cell Type B
     homo_num_cell_A: number of Cell Type A in the homogenous model
     connect_to_same_type: boolean for whether the two cells are of the same type
     """
     if connect_to_same_type:
-        probability = convergence / (homo_num_cell_A - 1)
+        probability = convergence / (homo_num_cell_A - 1) #don't want synapse to itself
     else:
         probability = convergence / homo_num_cell_A
     return probability
@@ -34,7 +34,8 @@ def homo_edge_probability(convergence, homo_num_cell_A, connect_to_same_type=Fal
 def homo_edge_probability_from_D(uni_connections, dist_num_cell_B, homo_num_cell_A, rec_connections=0, connect_to_same_type=False):
     """
     Calculates the probability of a given edge being created in a homogenous context from Cell Type A -> Cell Type B. If the connection is to a cell of the 
-    same type, then it accounts for not wanting a synapse to itself.
+    same type, then it accounts for not wanting a synapse to itself. If they are not in the same network (i.e. a virtual Cell Type A connecting to a 
+    biophysical Cell Type A), then this exception does not apply.
     uni_connections: number of unidirectional connections from Cell Type A -> Cell Type B
     dist_num_cell_B: number of Cell Type B in the distant dependent model
     homo_num_cell_A: number of Cell Type A in the homogenous model
